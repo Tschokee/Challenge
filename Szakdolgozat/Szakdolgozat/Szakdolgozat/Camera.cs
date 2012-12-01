670260a6b4866d5fb724c7d470c8024286c5600b
Tschokee <fre-mail@freemail.hu>
2012-12-01 11:31:36 +0100

    add: Camera.cs
    abstract camera class

---
+++ Szakdolgozat/Szakdolgozat/Szakdolgozat/Camera.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Szakdolgozat
+{
+    public abstract class Camera
+    {
+        public Matrix View { get; set; }
+        public Matrix Projection { get; set; }
+        protected GraphicsDevice GraphicsDevice { get; set; }
+
+        public Camera(GraphicsDevice graphicsDevice)
+        {
+            this.GraphicsDevice = graphicsDevice;
+            generatePerspectiveProjectionMatrix(MathHelper.PiOver4);
+        }
+
+        private void generatePerspectiveProjectionMatrix(float FieldOfView)
+        {
+            PresentationParameters pp = GraphicsDevice.PresentationParameters;
+            float aspectRatio = (float)pp.BackBufferWidth / (float)pp.BackBufferHeight;
+            this.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45),
+                                                                    aspectRatio, 
+                                                                    0.1f, 
+                                                                    1000000.0f);
+        }
+
+        public virtual void Update()
+        {
+        }
+    }
+}
